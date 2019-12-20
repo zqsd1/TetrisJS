51cf7abcc19c9d7a74d3bbcf314e4ddd38220534
yann marande <yann.marande@sfr.fr>
2019-12-20 12:27:24 +0100

    creation de cubes dans un tableau qui servira a etre dessiner

--- scripts/tetris.js
+++ scripts/tetris.js
@@ -1,3 +1,14 @@
+class Cube{
+    constructor(width,height,x,y,couleur) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y =y;
+        this.couleur = couleur;
+    }
+}
+
+
 //param du tetris
 var nbCubeX = 10;
 var nbCubeY = 20;
@@ -19,8 +30,24 @@ function dessiner(params) {
     if (canvasTetris.getContext){
     var dessinateur = canvasTetris.getContext("2d");
 
+    tableauCubes.forEach(cube => {
+   
+        dessinateur.fillStyle = cube.couleur;
+        dessinateur.fillRect(cube.x*cubeWidth,cube.y*cubeHeight,cube.width,cube.height);
+            console.log("hello");
+        
+    });
     }
 }
+
+
+var tableauCubes = new Array(nbCubeX*nbCubeY);
+
+var tmp = createPiece();
+tableauCubes[4] = tmp;
+
+console.log(tableauCubes);
+dessiner();
 
 //pour agir sur les action utilisateur
 document.onKeyDown = inputUser;
@@ -45,7 +72,7 @@ function demarrer(params) {
     //pour eviter de lancer plusieur fois
     if (!isPlay) {
         isPlay = !isPlay;
-        window.setInterval(deplacement,tempo,/*arg +1Y */)
+        window.setInterval(deplacement,tempo/*,arg +1Y */)
 
     }
 
@@ -58,4 +85,9 @@ function demarrer(params) {
  */
 function deplacement(params) {
     
+}
+
+function createPiece(params) {
+    var cubeTest = new Cube(cubeWidth,cubeHeight,5,0,"red");
+    return cubeTest;
 }
