d0e6d293f2451f0f5a0c702fd5ba5dd318392acb
zqsd <17475349+zqsd1@users.noreply.github.com>
2019-12-23 18:03:34 +0100

    oublie class cube

---
+++ scripts/cubeClass.js
@@ -0,0 +1,11 @@
+class Cube {
+    constructor(width, height, x, y, couleur, isActive = false) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.couleur = couleur;
+        this.isActive = isActive;
+
+    }
+}
